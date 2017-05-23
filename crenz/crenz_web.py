1ac988242cf97c3e9a69b131c13fe9a26900cb2f
Masahiko Kitamura <ktmrmshk@gmail.com>
2017-05-23 18:18:30 +0900

    ver0.0.2: supporing ajax

--- crenz/crenz_web.py
+++ crenz/crenz_web.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, Markup, request
 import sys
+import json
 from crenz.dns_tester import DnsTester
+import time
 
 app=Flask(__name__)
 
-@app.route('/', methods=['GET'])
-@app.route('/index.html')
 def index():
   pagedata={'zone':'', 'authns':'', 'zonefile':''}
   pagedata['console']='get method'
@@ -15,7 +15,7 @@ def index():
   return render_template('crenz.html', pd=pagedata)
 
 
-@app.route('/', methods=['POST'])
+#@app.route('/', methods=['POST'])
 def recv_post():
   pagedata={}
   pagedata['authns']=request.form['authns']
@@ -43,7 +43,52 @@ def recv_post():
   pagedata['table']=ret
   #pagedata['table']=[(True,2,3), (False,4,5)]
   return render_template('crenz.html', pd=pagedata)
+ 
+@app.route('/api/hello/')
+def apidemo():
+  ret={}
+  ret['name']='Masa'
+  ret['age']=12345
+  return json.dumps(ret)
+
+@app.route('/', methods=['GET'])
+@app.route('/index.html')
+@app.route('/test/')
+def index2():
+  pagedata={'zone':'', 'authns':'', 'zonefile':''}
+  pagedata['console']='get method'
+  #pagedata['table']=[(True,2,3), (False,4,5)]
+  pagedata['table']=[]
+  pagedata['action'] = '/'
+  return render_template('crenz_ajax.html', pd=pagedata)
+
+
+@app.route('/', methods=['POST'])
+@app.route('/test/', methods=['POST'])
+def post2():
+  pagedata={}
+  pagedata['authns']=request.form['authns']
+  pagedata['zone']=request.form['zone']
+  pagedata['zonefile']=request.form['zonefile']
+  pagedata['action'] = '/'
   
+  try:
+    zonename=None
+    if request.form['zone'] != '':
+      zonename = request.form['zone']
+    dt = DnsTester( request.form['authns'], zonename)
+    dt.load_txt( pagedata['zonefile'].replace('\r\n', '\n') )
+    #pagedata['console']='done'
+    ret = dt.zone_check()
+  except Exception as err:
+    pagedata['console']= 'err: {}\n{}'.format(err, sys.exc_info())  
+    ret = []
+
+  pagedata['table']=ret
+  #return render_template('crenz.html', pd=pagedata)
+  #time.sleep(1)
+  #return json.dumps(request.form)
+  return json.dumps(pagedata)
 
 if __name__ == "__main__":
   app.run(debug = True)
