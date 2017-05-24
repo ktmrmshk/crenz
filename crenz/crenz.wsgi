0238cb12985385b532750fbb8c0439c1bbfe579e
root <root@ip-172-31-16-117.ap-northeast-1.compute.internal>
2017-05-24 05:18:28 +0000

    added conf files for wsgi on apache2

---
+++ crenz/crenz.wsgi
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, '/var/www/crenz/')
+
+
+from crenz.crenz_web import app as application
+
--- crenz/crenz_web.py
+++ crenz/crenz_web.py
@@ -91,6 +91,6 @@ def post2():
   return json.dumps(pagedata)
 
 if __name__ == "__main__":
-  app.run(debug = True)
+  app.run(host='0.0.0.0', port=80,debug = True)
 
 
